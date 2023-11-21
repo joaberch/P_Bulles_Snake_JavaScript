b8a69ff95b799914b2170c212914ce025556517c
joaberch <122774888+joaberch@users.noreply.github.com>
2023-11-21 13:01:24 +0100

    Make the snake move with the arrow input

--- snake-js-game-main/src/main.js
+++ snake-js-game-main/src/main.js
@@ -1,12 +1,13 @@
 import '../css/style.css';
 
 //variable declaration
-let direction = 'D' //R = Right | L = Left | D = Down | U = Up
+let direction = 'R' //R = Right | L = Left | D = Down | U = Up
 let positionx = 3, positiony = 3;
 let speed = 250;
 
 let head = true;
-let longueursnake = 3;
+let snakeSize = 3;
+let snakeChunk = 0;
 
 //Making an array of array
 let x = new Array(16);
@@ -20,12 +21,10 @@ const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 const move = () => {
-
   //Dessine la grille de jeu
-  //ctx.fillStyle = 'black';
-  //ctx.fillRect(0, 0, 800, 800);
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, 800, 800);
   main();
-
   // Rafraichit a chaque seconde
   setTimeout(() => {
     requestAnimationFrame(move);
@@ -37,11 +36,28 @@ const move = () => {
 //alert(canvas);
 //alert(ctx);
 
+//////////////////////////////////////////////////    EVENT    ////////////////////////////////////////////////
+document.addEventListener('keydown', arrowclicked) //Change the direction
+//TODO : Pause menu with an alert
+
+
 
 //////////////////////////////////////////////////   Function   ///////////////////////////////////////////////
 function main() {
   drawAllSnake();
   checkDirection();
+  getallcoordinate()
+}
+
+function getallcoordinate() {
+  
+}
+
+function arrowclicked(event) {
+  if(event.keyCode == 37 && direction != 'R') {direction = 'L'}
+  else if (event.keyCode == 39 && direction != 'L') {direction = 'R'}
+  else if (event.keyCode == 38 && direction != 'D') {direction = 'U'}
+  else if (event.keyCode == 40 && direction != 'U') {direction = 'D'}
 }
 
 function checkDirection() { //TODO : border and check up/down
@@ -52,23 +68,29 @@ function checkDirection() { //TODO : border and check up/down
 }
 
 function drawAllSnake() {
-  for(let i = 0; i < longueursnake; ++i)
+  for(let i = 0; i < snakeSize; ++i)
   {
+    if (i == snakeSize-1)
+    {head=true}
+    else{head = false}
     drawSnakePart();
   }
+  head = false;
+  snakeChunk = 0;
 }
 
 function drawSnakePart() {
   ctx.beginPath();
-  ctx.lineWidth = "5";
+  ctx.lineWidth = 5;
   if (head) {
     ctx.strokeStyle = "Red"
   } else {
     ctx.strokeStyle = "black"
   }
-  head = false;
   ctx.fillStyle = "Red";
-  ctx.rect(positionx, positiony, 50, 50);
+  //TODO
+  ctx.rect(positionx+(snakeChunk*50), positiony, 50, 50);
+  ++snakeChunk
   ctx.stroke();
 }
 
