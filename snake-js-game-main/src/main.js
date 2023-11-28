d482405ab42caef61d54a4fd6a13beedffe8cd96
joaberch <122774888+joaberch@users.noreply.github.com>
2023-11-28 15:22:50 +0100

    Adding a game over screen

--- snake-js-game-main/src/main.js
+++ snake-js-game-main/src/main.js
@@ -1,64 +1,40 @@
 import '../css/style.css';
 
 //variable declaration
-let direction = 'R' //R = Right | L = Left | D = Down | U = Up
-let positionx = 3, positiony = 3;
 let speed = 200;
 
-let head = true;
-let snakeSize = 1;
-let framereset = 0;
-let snakeChunk = 0;
-let checkreplay = false;
-let checkapple = false; //Check if there's an apple in the game
-let randomcolumn
-let randomlist
+let randomx = getRandomInt(16);
+let randomy = getRandomInt(16);
+let positionx = 4;
+let positiony = 0;
+let direction = 'R';
+let CheckApple = false;
+let SnakeSize = 4;
+let checkGameIsRunning = true
 
-let cptrmap = 0
-let map = [
-  [++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap,], //1
-  [++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap,], //2
-  [++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap,], //3
-  [++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap,], //4
-  [++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap,], //5
-  [++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap,], //6
-  [++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap,], //7
-  [++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap,], //8
-  [++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap,], //9
-  [++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap,], //10
-  [++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap,], //11
-  [++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap,], //12
-  [++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap,], //13
-  [++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap,], //14
-  [++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap,], //15
-  [++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap, ++cptrmap,], //16
-];
+let restartButton = {
+  x: 250,
+  y: 400,
+  width: 300,
+  height: 100,
+}
 
-let once = true;
-let randomx;
-let randomy;
-//do {
+// The rectangle should have x,y,width,height properties
+var rect = {
+  x: 250,
+  y: 400,
+  width: 300,
+  height: 100,
+};
 
-//} while (randomx % 50 == 0)
-
-//do {
-//} while (randomy % 500 == 0)
-randomx = Math.floor(0 + Math.random()*(750 - 0 + 1));
-randomy = Math.floor(0 + Math.random()*(750 - 0 + 1));
-let V3positionx = 0;
-let V3positiony = 0;
-let V3direction = 'R';
-let V3SnakeTail = 0;
-let V3CheckApple = false;
-let V3arrayColumn = 0;
-let V3arrayList = 0;
+let snake = [{ x: 3, y: 0 }, { x: 2, y: 0 }, { x: 1, y: 0 }, { x: 0, y: 0 },]
 
 //const declaration
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 const arraySize = 15;
 
-//Making an array of array
+//Making an array of array 
 let arrayPlayground = new Array(arraySize);
 for (let i = 0; i < arraySize; ++i) { //0 = empty | 1 = snake | 2 = apple
   arrayPlayground[i] = new Array(arraySize)
@@ -69,245 +45,173 @@ for (let x = 0; x < arraySize; ++x) {
     arrayPlayground[x[y]] = 0
   }
 }
-arrayPlayground[V3arrayColumn[V3arrayList]] = 1
+arrayPlayground[0[0]] = 1
 
 const move = () => {
   //Dessine la grille de jeu
-  //if (framereset == snakeSize) {
-  //framereset = 0;
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, 800, 800);
-  //ctx.stroke;
-  //}
+
   main();
 
   // Rafraichit a chaque seconde
   setTimeout(() => {
     requestAnimationFrame(move);
   }, speed);
-  //head = true;
 };
 
 ///////////////////////////////////////////////////   Code   /////////////////////////////////////////////////
-//alert(canvas);
-//alert(ctx);
+
 
 //////////////////////////////////////////////////    EVENT    ////////////////////////////////////////////////
 document.addEventListener('keydown', arrowclicked) //Change the direction
 //TODO : Pause menu with an alert
 
-
-
 //////////////////////////////////////////////////   Function   ///////////////////////////////////////////////
 function main() {
-  //Version 1
-  /*generateApple();
-  DisplayApple();
-  //drawAllSnake();
-  drawSnakePart();
-  checkDirection();
-  getallcoordinate();
-  framereset++;
-  
-  if(positionx > 803 || positionx < -53 || positiony < -50 || positiony > 853)
-  {
-    gameOver()
-  }
-  */
-
-  //Version 2
-  /*
-  startAGame();
-  gridDisplay();
-  */
-
   //Version 3
-  drawSnake();
-  drawSnakeTail();
+  drawSnakeHead();
   V3generateApple();
   drawApple();
   moveSnake();
   collisionApple();
+
+  drawScore();
+  checkGameOver();
 }
 
-function collisionApple() {
-
+function gameInit() {
+  positionx = 4
+  positiony = 0
+  direction = "R"
+  SnakeSize = 4
 }
 
-function V3generateApple() {
-  if (V3CheckApple) {
-    while (randomx % 5000 == 0) {randomx = Math.floor(0 + Math.random()*(750 - 0 + 1))} //TODO
-    do {randomy = Math.floor(0 + Math.random()*(750 - 0 + 1));} while (randomx % 50 == 0) {}
-    V3CheckApple = false;
+function checkGameOver() {
+  if (positionx > 16 || positionx < -1 || positiony > 16 || positiony < -1) {
+    checkGameIsRunning = false
+
+    //draw a new background
+    ctx.fillStyle = 'purple'
+    ctx.fillRect(0, 0, 800, 800)
+
+    //Write the game over
+    ctx.fillStyle = 'black'
+    ctx.font = "100px Arial";
+    ctx.fillText("GAME OVER", 100, 300)
+
+    //Write the score
+    ctx.font = "30px Arial"
+    ctx.fillText(`Your score : ${SnakeSize - 4}`, 300, 350)
+
+    //draw the restart button
+    ctx.fillStyle = 'red'
+    ctx.fillRect(250, 400, 300, 100)
+
+    //draw the border of the restart button
+    ctx.strokeStyle = 'black'
+    ctx.strokeRect(250, 400, 300, 100)
+
+    //Write the restart in the button
+    ctx.fillStyle = 'black'
+    ctx.font = "60px Arial"
+    ctx.fillText("Restart", 300, 470)
+
+
   }
 }
 
-function drawSnakeTail() {
-  ctx.beginPath();
-  ctx.lineWidth = 5;
-  ctx.strokeStyle = "red";
-  ctx.fillStyle = "red";
-  ctx.fillRect(V3positionx, V3positiony, 50, 50);
-  ctx.stroke();
+function drawScore() {
+  ctx.font = "30px Arial";
+  ctx.fillText(SnakeSize - 4, 50, 60);
+}
+
+function collisionApple() {
+  if (positionx == randomx && positiony == randomy) {
+    CheckApple = true;
+    ++SnakeSize;
+    snake.push()
+  }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function V3generateApple() {
+  if (CheckApple) {
+    randomx = getRandomInt(16);
+    randomy = getRandomInt(16);
+    let applePosition = [randomx, randomy]
+    CheckApple = false;
+  }
 }
 
 function moveSnake() {
-  if(V3direction == 'R') {V3positionx += 50;} //Going right
-  else if (V3direction == 'L') {V3positionx -= 50;} //Going left
-  else if (V3direction == 'D') {V3positiony += 50} //Going up
-  else if (V3direction == 'U') {V3positiony -= 50} //Going down
+  if (direction == 'R') { let snakePosition = [++positionx, positiony] } //Going right
+  else if (direction == 'L') { let snakePosition = [--positionx, positiony] } //Going left
+  else if (direction == 'D') { let snakePosition = [positionx, ++positiony] } //Going up
+  else if (direction == 'U') { let snakePosition = [positionx, --positiony] } //Going down
 }
 
 function drawApple() {
   ctx.beginPath();
   ctx.lineWidth = 5;
-  ctx.strokeStyle = "green";
-  ctx.fillStyle = "green";
-  ctx.fillRect(randomx, randomy, 50, 50);
+  ctx.strokeStyle = "darkgreen";
+  ctx.fillStyle = "lightgreen";
+  ctx.fillRect((randomx * 50), (randomy * 50), 50, 50);
   ctx.stroke();
 }
 
-function drawSnake() {
-  ctx.beginPath();
-  ctx.lineWidth = 5;
-  ctx.strokeStyle = "red";
-  ctx.fillStyle = "red";
-  ctx.fillRect(V3positionx, V3positiony, 50, 50);
-  ctx.stroke();
-}
-
-function gridDisplay() {
-  for (let x = 0; x < arraySize + 1; ++x) {
-    for (let y = 0; y < arraySize + 1; ++y) {
-      if (arrayPlayground[x[y]] == 0) //Floor
-      {
-        ctx.beginPath();
-        ctx.lineWidth = 5;
-        ctx.strokeStyle = "black";
-        ctx.fillStyle = "black";
-        ctx.fillRect(x - 10, y - 10, 50, 50);
-        ctx.rect((x * 50) + 3, (y * 50) + 3, 50, 50);
-        ctx.stroke();
-      }
-      else if (arrayPlayground[x[y]] == 1) //Snake
-      {
-        ctx.beginPath();
-        ctx.lineWidth = 5;
-        ctx.strokeStyle = "red";
-        ctx.fillStyle = "red";
-        ctx.fillRect(x - 10, y - 10, 50, 50);
-        ctx.rect((x * 50) + 3, (y * 50) + 3, 50, 50);
-        ctx.stroke();
-      }
-      else if (arrayPlayground[x[y]] == 2) //Apple
-      {
-        ctx.beginPath();
-        ctx.lineWidth = 5;
-        ctx.strokeStyle = "green";
-        ctx.fillStyle = "green";
-        ctx.rect((x * 50) + 3, (y * 50) + 3, 50, 50);
-        ctx.stroke();
-      }
-    }
+function drawSnakeHead() {
+  for (let i = 0; i <= SnakeSize; ++i) {
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "darkred";
+    ctx.fillStyle = "red"; //TODO : find lightred
+    ctx.fillRect(positionx * 50, positiony * 50, 50, 50);
+    ctx.stroke();
   }
-}
-
-function startAGame() {
-  if (once) {
-    once = false
-
-    gridinit()
-    snakeinit()
-  }
-}
-
-function snakeinit() {
-  arrayPlayground[0[0]] = 1
-}
-
-function gridinit() {
-  for (let x = 0; x < arraySize; ++x) {
-    for (let y = 0; y < arraySize; ++y) {
-      arrayPlayground[x[y]] = 0
-    }
-  }
-}
-
-function generateApple() {
-  if (!checkapple) {
-    randomcolumn = Math.floor(1 + Math.random() * (15 - 1 + 1)); //colonne random
-    randomlist = Math.floor(1 + Math.random() * (15 - 1 + 1)); //liste random
-  }
-}
-
-function DisplayApple() {
-  ctx.beginPath();
-  ctx.lineWidth = 5;
-  ctx.strokeStyle = "Red";
-  ctx.fillStyle = "Red";
-  ctx.rect(3 + (randomcolumn * 50), 3 + (randomlist * 50), 50, 50);
-  ctx.stroke;
-}
-
-function gameOver() {
-  alert(`You are dead. You have made a score of ${snakeSize - 1}`)
-  checkreplay = confirm('Wanna retry?')
-  if (checkreplay) {
-    positionx = 3;
-    positiony = 3;
-    direction = 'R'
-  }
-}
-
-function getallcoordinate() {
-
 }
 
 function arrowclicked(event) {
-  //Version 1
+  //Version 3
   if (event.keyCode == 37 && direction != 'R') { direction = 'L' }
   else if (event.keyCode == 39 && direction != 'L') { direction = 'R' }
   else if (event.keyCode == 38 && direction != 'D') { direction = 'U' }
   else if (event.keyCode == 40 && direction != 'U') { direction = 'D' }
-
-  //Version 3
-  if (event.keyCode == 37 && V3direction != 'R') { V3direction = 'L' }
-  else if (event.keyCode == 39 && V3direction != 'L') { V3direction = 'R' }
-  else if (event.keyCode == 38 && V3direction != 'D') { V3direction = 'U' }
-  else if (event.keyCode == 40 && V3direction != 'U') { V3direction = 'D' }
 }
 
-function checkDirection() { //TODO : border and check up/down
-  if (direction == 'R') { positionx += 50 }
-  else if (direction == 'L') { positionx -= 50 }
-  else if (direction == 'U') { positiony -= 50 }
-  else { positiony += 50 }
+///////////////////////////////////////////////////////////////////////// Snippet ///////////////////////////////////////////////
+
+// Function to get the mouse position
+function getMousePos(canvas, event) {
+  var restartButton = canvas.getBoundingClientRect();
+  return {
+    x: event.clientX - restartButton.left,
+    y: event.clientY - restartButton.top,
+  };
 }
 
-function drawAllSnake() {
-  for (let i = 0; i < snakeSize; ++i) {
-    if (i == snakeSize - 1) { head = true }
-    else { head = false }
-    drawSnakePart();
+// Function to check whether a point is inside a rectangle
+function isInside(pos, restartButton) {
+  return pos.x > restartButton.x && pos.x < restartButton.x + restartButton.width && pos.y < restartButton.y + restartButton.height && pos.y > restartButton.y
+}
+
+// Binding the click event on the canvas
+canvas.addEventListener('click', function (evt) {
+  var mousePos = getMousePos(canvas, evt);
+  if (!checkGameIsRunning) {
+    if (isInside(mousePos, restartButton)) {
+      checkGameIsRunning = true
+      main()
+
+      //Making the variable go back to the init
+      gameInit()
+    }
   }
-  head = false;
-  snakeChunk = 0;
-}
+}, false);
 
-function drawSnakePart() {
-  ctx.beginPath();
-  ctx.lineWidth = 5;
-  if (head) {
-    ctx.strokeStyle = "Red"
-  } else {
-    ctx.strokeStyle = "black"
-  }
-  ctx.fillStyle = "Red";
-  //TODO
-  ctx.rect(positionx + (snakeChunk * 50), positiony, 50, 50);
-  ++snakeChunk
-  ctx.stroke();
-  snakeChunk = 0;
-}
+
 
 requestAnimationFrame(move);
