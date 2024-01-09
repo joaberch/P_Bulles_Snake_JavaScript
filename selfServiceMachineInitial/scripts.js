1432d47f2fcf0ef586e5ff20ad78472d78468e41
joaberch <122774888+joaberch@users.noreply.github.com>
2024-01-09 16:15:37 +0100

    Polish the service machine code

--- selfServiceMachineInitial/scripts.js
+++ selfServiceMachineInitial/scripts.js
@@ -112,68 +112,60 @@ function activate(nbr)
   {
     case 1:
       if (!checkBigMacIsActive) {
-        document.getElementById('quantity-area-1').style.visibility = 'visible'
         checkBigMacIsActive = true
-        moreBigMac()
+        more(nbr)
         }
       break
     case 2:
       if (!checkMcChickenIsActive) {
-        document.getElementById('quantity-area-2').style.visibility = 'visible'
         checkMcChickenIsActive = true
-        moreMcChicken()
+        more(nbr)
         }
       break
     case 3:
       if (!checkDoubleCheeseBurgerIsActive) {
-        document.getElementById('quantity-area-3').style.visibility = 'visible'
         checkDoubleCheeseBurgerIsActive = true
-        moreDoubleCheeseBurger()
+        more(nbr)
         }
       break
     case 4:
       if (!checkFriesIsActive) {
-        document.getElementById('quantity-area-4').style.visibility = 'visible'
         checkFriesIsActive = true
-        moreFries()
+        more(nbr)
         }
       break
     case 5:
       if (!checkMcNuggetsIsActive) {
-        document.getElementById('quantity-area-5').style.visibility = 'visible'
         checkMcNuggetsIsActive = true
-        moreMcNuggets()
+        more(nbr)
         }
       break
     case 6:
       if (!checkSaladIsActive) {
-        document.getElementById('quantity-area-6').style.visibility = 'visible'
         checkSaladIsActive = true
-        moreSalad()
+        more(nbr)
         }
       break
     case 7:
       if (!checkCokeIsActive) {
-        document.getElementById('quantity-area-7').style.visibility = 'visible'
         checkCokeIsActive = true
-        moreCoke()
+        more(nbr)
         }
       break
     case 8:
       if (!checkIceTeaIsActive) {
-        document.getElementById('quantity-area-8').style.visibility = 'visible'
         checkIceTeaIsActive = true
-        moreIceTea()
+        more(nbr)
         }
       break
     case 9:
       if (!checkWaterIsActive) {
-        document.getElementById('quantity-area-9').style.visibility = 'visible'
         checkWaterIsActive = true
-        moreWater()
+        more(nbr)
         }
       break
   }
+  document.getElementById('quantity-area-' + nbr).style.visibility = 'visible'
 }
 
 //desactivate all the products
@@ -213,168 +205,130 @@ function desactivate(nbr)
 }
 
 //All the function about + and - button
-function moreBigMac()
+function more(nbr)
 {
-  document.getElementById('bigMacNumber').innerText = ++bigMacNumber
-  totalSum += bigMacPrice
-  document.getElementById('bigMac').style.borderColor = 'blue'
-  displayTotalValue()
-}
-
-function lessBigMac()
-{
-  if (bigMacNumber > 0){
-  document.getElementById('bigMacNumber').innerText = --bigMacNumber
-  totalSum -= bigMacPrice
-  if (bigMacNumber == 0) {
-    document.getElementById('bigMac').style.borderColor = 'black'
-    setTimeout(desactivate('1'),1)
+  switch (nbr)
+  {
+    case 1:
+      document.getElementById('bigMacNumber').innerText = ++bigMacNumber
+      totalSum += bigMacPrice
+      document.getElementById('bigMac').style.borderColor = 'blue'
+      break
+    case 2:
+      document.getElementById('McChickenNumber').innerText = ++mcChickenNumber
+      totalSum += mcChickenPrice
+      document.getElementById('McChicken').style.borderColor = 'blue'
+      break
+    case 3:
+      document.getElementById('DoubleCheeseBurgerNumber').innerText = ++doubleCheeseBurgerNumber
+      totalSum += doubleCheeseBurgerPrice
+      document.getElementById('DoubleCheeseBurger').style.borderColor = 'blue'
+      break
+    case 4:
+      document.getElementById('FriesNumber').innerText = ++friesNumber
+      totalSum += friesPrice
+      document.getElementById('Fries').style.borderColor = 'blue'
+      break
+    case 5:
+      document.getElementById('McNuggetsNumber').innerText = ++mcNuggetsNumber
+      totalSum += mcNuggetsPrice
+      document.getElementById('McNuggets').style.borderColor = 'blue'
+      break
+    case 6:
+      document.getElementById('SaladNumber').innerText = ++saladNumber
+      totalSum += saladPrice
+      document.getElementById('Salad').style.borderColor = 'blue'
+      break
+    case 7:
+      document.getElementById('CokeNumber').innerText = ++cokeNumber
+      totalSum += cokePrice
+      document.getElementById('Coke').style.borderColor = 'blue'
+      break
+    case 8:
+      document.getElementById('IceTeaNumber').innerText = ++iceTeaNumber
+      totalSum += iceTeaPrice
+      document.getElementById('IceTea').style.borderColor = 'blue'
+      break
+    case 9:
+      document.getElementById('WaterNumber').innerText = ++waterNumber
+      totalSum += waterPrice
+      document.getElementById('Water').style.borderColor = 'blue'
+      break
   }
   displayTotalValue()
+}
+
+function less(nbr)
+{
+  switch (nbr)
+  {
+    case 1:
+      if (bigMacNumber > 0){
+        document.getElementById('bigMacNumber').innerText = --bigMacNumber
+        totalSum -= bigMacPrice
+        if (bigMacNumber == 0) {
+          document.getElementById('bigMac').style.borderColor = 'black'
+          setTimeout(desactivate(1),1)
+        }
+      }
+      break
+    case 2:
+      if (mcChickenNumber > 0) {
+        document.getElementById('McChickenNumber').innerText = --mcChickenNumber
+        totalSum -= mcChickenPrice
+        if (mcChickenNumber == 0) {document.getElementById('McChicken').style.borderColor = 'black'; setTimeout(desactivate('2'),1)}
+      }
+      break
+    case 3:
+      if (doubleCheeseBurgerNumber > 0) {
+        document.getElementById('DoubleCheeseBurgerNumber').innerText = --doubleCheeseBurgerNumber
+        totalSum -= doubleCheeseBurgerPrice
+        if (doubleCheeseBurgerNumber == 0) {document.getElementById('DoubleCheeseBurger').style.borderColor = 'black'; setTimeout(desactivate('3'),1)}
+      }
+      break
+    case 4:
+      if (friesNumber > 0) {
+        document.getElementById('FriesNumber').innerText = --friesNumber
+        totalSum -= friesPrice
+        if (friesNumber == 0) {document.getElementById('Fries').style.borderColor = 'black'; setTimeout(desactivate('4'),1)}
+      }
+      break
+    case 5:
+      if (mcNuggetsNumber > 0) {
+        document.getElementById('McNuggetsNumber').innerText = --mcNuggetsNumber
+        totalSum -= mcNuggetsPrice
+        if (mcNuggetsNumber == 0) {document.getElementById('McNuggets').style.borderColor = 'black'; setTimeout(desactivate('5'),1)}
+      }
+      break
+    case 6:
+      if (saladNumber > 0) {
+        document.getElementById('SaladNumber').innerText = --saladNumber
+        totalSum -= saladPrice
+        if (saladNumber == 0) {document.getElementById('Salad').style.borderColor = 'black'; setTimeout(desactivate('6'),1)}
+      }
+      break
+    case 7:
+      if (cokeNumber > 0) {
+        document.getElementById('CokeNumber').innerText = --cokeNumber
+        totalSum -= cokePrice
+        if (cokeNumber == 0) {document.getElementById('Coke').style.borderColor = 'black'; setTimeout(desactivate('7'),1)}
+      }
+      break
+    case 8:
+      if (iceTeaNumber > 0) {
+        document.getElementById('IceTeaNumber').innerText = --iceTeaNumber
+        totalSum -= iceTeaPrice
+        if (iceTeaNumber == 0) {document.getElementById('IceTea').style.borderColor = 'black'; setTimeout(desactivate('8'),1)}
+      }
+      break
+    case 9:
+      if (waterNumber > 0) {
+        document.getElementById('WaterNumber').innerText = --waterNumber
+        totalSum -= waterPrice
+        if (waterNumber == 0) {document.getElementById('Water').style.borderColor = 'black'; setTimeout(desactivate('9'),1)}
+      }
+      break
   }
-}
-
-function lessMcChicken()
-{
-  if (mcChickenNumber > 0) {
-  document.getElementById('McChickenNumber').innerText = --mcChickenNumber
-  totalSum -= mcChickenPrice
-  if (mcChickenNumber == 0) {document.getElementById('McChicken').style.borderColor = 'black'; setTimeout(desactivate('2'),1)}
-  displayTotalValue()
-  }
-}
-
-function moreMcChicken()
-{
-  document.getElementById('McChickenNumber').innerText = ++mcChickenNumber
-  totalSum += mcChickenPrice
-  document.getElementById('McChicken').style.borderColor = 'blue'
-  displayTotalValue()
-}
-
-function lessDoubleCheeseBurger()
-{
-  if (doubleCheeseBurgerNumber > 0) {
-  document.getElementById('DoubleCheeseBurgerNumber').innerText = --doubleCheeseBurgerNumber
-  totalSum -= doubleCheeseBurgerPrice
-  if (doubleCheeseBurgerNumber == 0) {document.getElementById('DoubleCheeseBurger').style.borderColor = 'black'; setTimeout(desactivate('3'),1)}
-  displayTotalValue()
-  }
-}
-
-function moreDoubleCheeseBurger()
-{
-  document.getElementById('DoubleCheeseBurgerNumber').innerText = ++doubleCheeseBurgerNumber
-  totalSum += doubleCheeseBurgerPrice
-  document.getElementById('DoubleCheeseBurger').style.borderColor = 'blue'
-  displayTotalValue()
-}
-
-function lessFries()
-{
-  if (friesNumber > 0) {
-  document.getElementById('FriesNumber').innerText = --friesNumber
-  totalSum -= friesPrice
-  if (friesNumber == 0) {document.getElementById('Fries').style.borderColor = 'black'; setTimeout(desactivate('4'),1)}
-  displayTotalValue()
-  }
-}
-
-function moreFries()
-{
-  document.getElementById('FriesNumber').innerText = ++friesNumber
-  totalSum += friesPrice
-  document.getElementById('Fries').style.borderColor = 'blue'
-  displayTotalValue()
-}
-
-function lessMcNuggets()
-{
-  if (mcNuggetsNumber > 0) {
-  document.getElementById('McNuggetsNumber').innerText = --mcNuggetsNumber
-  totalSum -= mcNuggetsPrice
-  if (mcNuggetsNumber == 0) {document.getElementById('McNuggets').style.borderColor = 'black'; setTimeout(desactivate('5'),1)}
-  displayTotalValue()
-  }
-}
-
-function moreMcNuggets()
-{
-  document.getElementById('McNuggetsNumber').innerText = ++mcNuggetsNumber
-  totalSum += mcNuggetsPrice
-  document.getElementById('McNuggets').style.borderColor = 'blue'
-  displayTotalValue()
-}
-
-function lessSalad()
-{
-  if (saladNumber > 0) {
-  document.getElementById('SaladNumber').innerText = --saladNumber
-  totalSum -= saladPrice
-  if (saladNumber == 0) {document.getElementById('Salad').style.borderColor = 'black'; setTimeout(desactivate('6'),1)}
-  displayTotalValue()
-  }
-}
-
-function moreSalad()
-{
-  document.getElementById('SaladNumber').innerText = ++saladNumber
-  totalSum += saladPrice
-  document.getElementById('Salad').style.borderColor = 'blue'
-  displayTotalValue()
-}
-
-function lessCoke()
-{
-  if (cokeNumber > 0) {
-  document.getElementById('CokeNumber').innerText = --cokeNumber
-  totalSum -= cokePrice
-  if (cokeNumber == 0) {document.getElementById('Coke').style.borderColor = 'black'; setTimeout(desactivate('7'),1)}
-  displayTotalValue()
-  }
-}
-
-function moreCoke()
-{
-  document.getElementById('CokeNumber').innerText = ++cokeNumber
-  totalSum += cokePrice
-  document.getElementById('Coke').style.borderColor = 'blue'
-  displayTotalValue()
-}
-
-function lessIceTea()
-{
-  if (iceTeaNumber > 0) {
-  document.getElementById('IceTeaNumber').innerText = --iceTeaNumber
-  totalSum -= iceTeaPrice
-  if (iceTeaNumber == 0) {document.getElementById('IceTea').style.borderColor = 'black'; setTimeout(desactivate('8'),1)}
-  displayTotalValue()
-  }
-}
-
-function moreIceTea()
-{
-  document.getElementById('IceTeaNumber').innerText = ++iceTeaNumber
-  totalSum += iceTeaPrice
-  document.getElementById('IceTea').style.borderColor = 'blue'
-  displayTotalValue()
-}
-
-function lessWater()
-{
-  if (waterNumber > 0) {
-  document.getElementById('WaterNumber').innerText = --waterNumber
-  totalSum -= waterPrice
-  if (waterNumber == 0) {document.getElementById('Water').style.borderColor = 'black'; setTimeout(desactivate('9'),1)}
-  displayTotalValue()
-  }
-}
-
-function moreWater()
-{
-  document.getElementById('WaterNumber').innerText = ++waterNumber
-  totalSum += waterPrice
-  document.getElementById('Water').style.borderColor = 'blue'
   displayTotalValue()
 }
 
